19fe6973b28be43a2743a312904b6c75e68f9788
shu <me@wadza.fr>
2019-06-11 11:52:44 +0200

    Herit

---
+++ src/components/input/SettingHeritBoolean.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+extends model
+
+block input
+  input(:id="id" ref="input" type="checkbox" :checked="value" @change.stop="handleChange")
+</template>
+
+<script lang="ts">
+import { Component, Prop } from 'vue-property-decorator'
+
+import BaseSetting from './BaseSetting'
+
+@Component
+export default class SettingHeritBoolean extends BaseSetting {
+
+  @Prop(Boolean)
+  readonly value!: boolean | undefined
+
+  mounted () {
+    const input: any = this.$refs.input
+    input.indeterminate = this.value === undefined
+  }
+
+  handleChange() {
+    this.sendChange(this.value === false ? undefined : !this.value)
+  }
+
+}
+</script>
--- src/components/service/BaseService.ts
+++ src/components/service/BaseService.ts
@@ -2,13 +2,14 @@ import { Component, Prop, Watch } from 'vue-property-decorator'
 
 import { Auth } from '@/types/App'
 import SettingBoolean from '../input/SettingBoolean.vue'
+import SettingHeritBoolean from '../input/SettingHeritBoolean.vue'
 import SettingInt from '../input/SettingInt.vue'
 import SettingSelect from '../input/SettingSelect.vue'
 import SettingString from '../input/SettingString.vue'
 import ServiceEmiter from '../ServiceEmiter'
 import ServiceHeader from '../ServiceHeader.vue'
 
-@Component({ components: { ServiceHeader, SettingString, SettingInt, SettingBoolean, SettingSelect } })
+@Component({ components: { ServiceHeader, SettingString, SettingInt, SettingBoolean, SettingHeritBoolean, SettingSelect } })
 export default class BaseService extends ServiceEmiter {
 
   @Prop({
